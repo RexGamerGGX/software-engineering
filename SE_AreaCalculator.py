9108e4d3be5b5faf91cc497b44dfb6e0df3eb1c8
Gabriel Solarte <rex.gamer.gg.alive@gmail.com>
2026-03-08 21:54:04 +1100

    Created area calculator file

---
+++ SE_AreaCalculator.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+# This function displays a menu to select a shape for area calculation.
+
+def AreaMenu():
+    
+    print("Which shape area do you wish to calculate?")
+    print("1. Rectangle")
+    print("2. Triangle")
+    print("3. Circle")
+
+    user_area_selection = int(input("Input the number of your selection: "))
+    
